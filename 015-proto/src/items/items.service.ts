2e6d8171664b4c5e3e4dc0475990809229b21a94
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-13 08:16:38 +0300

    add

---
+++ 015-proto/src/items/items.service.ts
@@ -0,0 +1,31 @@
+import { BaseItem, Item } from "./item.interface";
+import { Items } from "./items.interface";
+
+//In-Memory Store
+let items: Items = {
+  1: {
+    id: 1,
+    name: "Burger",
+    price: 599,
+    description: "Tasty",
+    image: "https://cdn.auth0.com/blog/whatabyte/burger-sm.png"
+  },
+  2: {
+    id: 2,
+    name: "Pizza",
+    price: 299,
+    description: "Cheesy",
+    image: "https://cdn.auth0.com/blog/whatabyte/pizza-sm.png"
+  },
+  3: {
+    id: 3,
+    name: "Tea",
+    price: 199,
+    description: "Informative",
+    image: "https://cdn.auth0.com/blog/whatabyte/tea-sm.png"
+  }
+};
+
+//Service Methods
+ export const findAll = async (): Promise<Item[]> => Object.values(items);
+ export const find = async (id: number): Promise<Item> => items[id];
