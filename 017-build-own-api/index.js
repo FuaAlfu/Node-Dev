e630a4feb8117a7283359707bbfd0774ba05e0c1
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-11-05 16:57:14 +0300

    make it better

--- 017-build-own-api/index.js
+++ 017-build-own-api/index.js
@@ -19,7 +19,32 @@ const newspapers = [{
     name: 'telegraph',
     address: 'https://www.telegraph.co.uk/climate-change',
     base: 'https://www.telegraph.co.uk'
-}
+},
+{
+    name: 'bbc',
+    address: 'https://www.bbc.co.uk/news/scince_and_environment',
+    base: 'https://www.bbc.co.uk',
+},
+{
+    name: 'es',
+    address: 'https://www.standard.co.uk/topic/climate-change',
+    base: 'https://www.standard.co.uk',
+},
+{
+    name: 'sun',
+    address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
+    base: '',
+},
+{
+    name: 'dm',
+    address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+    base: '',
+},
+{
+    name: 'nyp',
+    address: 'https://nypost.com/tag/climate-change',
+    base: '',
+},
 ]
 
 //create an empty array
@@ -52,7 +77,7 @@ app.get('/news', (req,res) =>{
     res.json(articles)
 });
 
-app.get('/news/:newspaperId', async (req,res) => {
+app.get('/news/:newspaperId', (req,res) => {  //async (req,res) => {
     //console.log(req.params.newspaperId);  //for testing inside the terminal..
    const newspaperId = req.params.newspaperId
    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
